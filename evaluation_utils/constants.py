ee715fe8b55cfc21957b911781532c9cd15bb130
Daniel Ayala <daniel.arayala@gmail.com>
2022-11-05 17:17:46 -0300

    Created constants file with the mapping of the known models used for evaluation

---
+++ evaluation_utils/constants.py
@@ -0,0 +1,11 @@
+
+# Models being evaluated from the timm library
+# Maps a shorthand name with the timm model identifier
+KNOWN_MODELS = {
+    'ViT-B_16': 'vit_base_patch16_224_in21k',
+    'ViT-S_16': 'vit_small_patch16_224_in21k',
+    'ViT-Ti_16': 'vit_tiny_patch16_224_in21k',
+    'DeiT-B_16': 'deit_base_patch16_224',
+    'DeiT-S_16': 'deit_small_patch16_224',
+    'DeiT-Ti_16': 'deit_tiny_patch16_224'
+}
