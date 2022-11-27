f21f0821d74165a4fab04efe33d81b9a3d382a88
Daniel Ayala <daniel.arayala@gmail.com>
2022-11-27 16:42:30 -0300

    Fixed problem with probs during evaluation of ViT

--- train_vit.py
+++ train_vit.py
@@ -63,7 +63,7 @@ def valid(args, model, writer, test_loader, global_step):
     logger.info("  Batch size = %d", args.eval_batch_size)
 
     model.eval()
-    all_preds, all_labels = [], []
+    all_preds, all_labels, all_probs = [], [], []
     epoch_iterator = tqdm(test_loader,
                           desc="Validating... (loss=X.X)",
                           bar_format="{l_bar}{r_bar}",
@@ -77,21 +77,15 @@ def valid(args, model, writer, test_loader, global_step):
             eval_loss = loss_fct(logits, y)
             eval_losses.update(eval_loss.item())
             preds = torch.argmax(logits, dim=-1)
-
-        if len(all_preds) == 0:
-            all_preds.append(preds.detach().cpu().numpy())
-            all_labels.append(y.detach().cpu().numpy())
-        else:
-            all_preds[0] = np.append(
-                all_preds[0], preds.detach().cpu().numpy(), axis=0
-            )
-            all_labels[0] = np.append(
-                all_labels[0], y.detach().cpu().numpy(), axis=0
-            )
+            probs = F.softmax(logits)
+            # Appending prediction, labels, and probabilities
+            all_preds.append(preds.cpu().numpy())
+            all_labels.append(y.cpu().numpy())
+            all_probs.append(probs.cpu().numpy())
         epoch_iterator.set_description("Validating... (loss=%2.5f)" % eval_losses.val)
 
     all_preds, all_labels = all_preds[0], all_labels[0]
-    val_metrics = get_classification_metrics(all_labels, all_preds)
+    val_metrics = get_classification_metrics(all_labels, all_preds, all_probs)
     log_evaluation(global_step, val_metrics, writer, 'val')
     writer.add_scalar("loss/val", scalar_value=eval_losses.avg, global_step=global_step)
     try:
