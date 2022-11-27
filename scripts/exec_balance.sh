f2a4772f320ddd502c2dc8abd812d57e4dcbdfe5
Francisco Malaguth <francisco.malaguth@dtidigital.com.br>
2022-11-27 17:49:05 -0300

    Feat: Update scripts for each config instead of grouped configs

---
+++ scripts/exec_balance.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+python train.py --name balance --model_arch BiT --model_type BiT-M-R50x1 --dataset eyepacs --num_steps 500 --num_workers 2 --learning_rate 0.003 --img_size 256 --batch_split 32 --train_batch_size 512 --eval_batch_size 128 --eval_every 10 --num_classes 5 --balance_classes --use_clearml
+python evaluate.py --name balance --dataset eyepacs --model_arch BiT --model_type BiT-M-R50x1 --img_size 256 --batch_size 512 --num_workers 2 --num_classes 5 --use_clearml
+
+python train.py --name balance --model_arch ViT --model_type ViT-S_16 --dataset eyepacs --warmup_steps 200 --num_steps 500 --num_workers 2 --learning_rate 0.03 --img_size 256 --batch_split 32 --train_batch_size 512 --eval_batch_size 128 --eval_every 10 --num_classes 5 --balance_classes --use_clearml
+python evaluate.py --name balance --dataset eyepacs --model_arch ViT --model_type ViT-S_16 --img_size 256 --batch_size 512 --num_workers 2 --num_classes 5 --use_clearml
